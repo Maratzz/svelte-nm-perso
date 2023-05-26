a5b7a7dcbcef22b481420111576771554faca9fe
Maratzz <nicolas.moisson@protonmail.com>
2023-05-26 17:07:53 +0200

    combined data url and session

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,11 @@
 export const prerender = true;
+
+export async function load({ data, url }) {
+
+  const currentRoute = url.pathname
+
+  return {
+    ...data,
+    currentRoute
+  }
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,18 @@
 <script>
-  import "$lib/styles/code_formatter.css"
-  import "$lib/styles/styles.scss"
-  import Header from "$lib/components/Header.svelte"
-  import Footer from "$lib/components/Footer.svelte"
+  import '$lib/styles/code_formatter.css'
+  import '$lib/styles/styles.scss'
+  import Header from '$lib/components/Header.svelte'
+  import Footer from '$lib/components/Footer.svelte'
+  import { fade } from 'svelte/transition'
+
+  export let data
 </script>
 
 <Header/>
 
-<main>
-  <slot />
-</main>
-
+{#key data.currentRoute}
+  <main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+    <slot />
+  </main>
+{/key}
 <Footer/>
