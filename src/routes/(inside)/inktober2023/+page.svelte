ff62e032040503026cc7a3ef0a62f093beed2c83
Maratz <nicolas.moisson@protonmail.com>
2024-06-27 11:01:19 +0200

    added details on the english summary for CD submission

--- src/routes/(inside)/inktober2023/+page.svelte
+++ src/routes/(inside)/inktober2023/+page.svelte
@@ -21,7 +21,7 @@
 
   <p class="alert alert-secondary">Voici un aperçu de mon travail d'écriture en cours. A mi-chemin entre critique, poésie et littérature ergodique, j'utilise <a href="https://images.squarespace-cdn.com/content/v1/5af1bd791aef1d143f85e67e/79397039-375a-44d9-bd12-e5a1fe566c4f/2023promptlist.jpg?format=2500w">chaque mot de l'Inktober 2023</a> comme point de départ pour produire des textes autour de certaines oeuvres vidéoludiques, ou des aspects de cette culture et de cette industrie. J'espère à terme pouvoir compiler les 31 textes dans un recueil disponible à la vente sur ce site, à prix libre. En attendant, bonne lecture !</p>
 
-  <p class="alert alert-primary">🇬🇧 English-speaking readers 🇬🇧, you can download <a href={saddle_english} download>the translated script right here</a> to follow along. I did my best to convey most of the meaning, but some may have been lost in translation 🙏🏽 Thanks for reading!</p>
+  <p class="alert alert-primary">🇬🇧 English-speaking readers 🇬🇧, here's a glimpse of the project I'm currently working on. Using the Inktober 2023 words as starting points, I'm writing French texts which are at the intersection of ergodic literature, poetry and criticism, with a good amount of video games on top of it. I hope to release all the pieces later this year on this very website, in a compilation available for sale on a pay-what-you-want basis to promote my work and hopefully produce something people will enjoy reading and ponder about. The text below is for the 18th word of the Inktober, which was <i>saddle</i>. You can download <a href={saddle_english} download="saddle_english_translation.txt">the translated script right here</a> to follow along. I did my best to convey most of the meaning, but some may have been lost in translation 🙏🏽 Thanks for reading!</p>
 
   <MediaQuery query="(min-height: 750px) and (min-width: 500px)" let:matches>
     {#if matches}
@@ -33,7 +33,7 @@
         height="950">
       </object>
     {:else}
-      <p class="mobile">Téléchargez le PDF <a href={saddle} download>en cliquant juste ici</a> et ouvrez-le avec l'application de votre choix sur téléphone</p>
+      <p class="mobile">Téléchargez le PDF <a href={saddle} download="saddle_nico_moisson_inktober_2023.pdf">en cliquant juste ici</a> et ouvrez-le avec l'application de votre choix sur téléphone</p>
     {/if}
   </MediaQuery>
 
