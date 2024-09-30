977ab0a03b6021cbb5324eb55b3318ccaf6bed9c
Maratz <nicolas.moisson@protonmail.com>
2024-09-30 19:21:13 +0200

    ✨[slug] routing in collection

---
+++ src/routes/(inside)/collection/[slug]/+page.js
@@ -0,0 +1,14 @@
+export async function load({ params, parent }) {
+  const { supabase, currentRoute } = await parent()
+
+  let slug = params.slug
+
+  let { data: item } = await supabase
+    .from("collection")
+    .select("*")
+    .eq('slug', slug)
+
+  return {
+    item: item[0] ?? []
+   }
+}
---
+++ src/routes/(inside)/collection/[slug]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let data
+
+  let { item } = data
+</script>
+
+<h2>{item.name}</h2>
+<p>Un type de {item.developers}, sorti le {item.date_relased}</p>
+{#if item.platform}
+  {item.platform}
+{/if}
+
+
+<img src={item.cover} alt="">
+
+{#if item.notes}
+  <p>{item.notes}</p>
+{/if}
