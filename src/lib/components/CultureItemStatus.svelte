00d41c4dc877a2ccc20d5fde36c19ff153dd0cbb
Maratzz <nicolas.moisson@protonmail.com>
2024-10-01 19:57:52 +0200

    💄 added CSS classes + new component

---
+++ src/lib/components/CultureItemStatus.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let item
+</script>
+
+{#if item.status === "wishlist"}
+  <span>📋</span>
+{:else if item.status === "backlog"}
+  <span>🎒 {#if item.type === "BD" | item.type === "série"}acquise{:else}acquis{/if}</span>
+{:else if item.status === "currently playing"}
+  <span>🔁 en cours</span>
+{:else if item.status === "finished"}
+  <span>✅ {#if item.type === "BD" | item.type === "série"}terminée{:else}terminé{/if}</span>
+{:else if item.status === "flushed"}
+  <span>💩 {#if item.type === "BD" | item.type === "série"}abandonnée{:else}abandonné{/if}</span>
+{/if}
+
