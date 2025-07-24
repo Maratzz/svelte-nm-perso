e2af9d79c7c415335047e5cd7c5f9693b688e665
Maratzz <nicolas.moisson@protonmail.com>
2025-07-25 00:29:09 +0200

    🚧 leaflet wip, data now available wherever I want, need styling and design

--- src/lib/components/LeafletMap.svelte
+++ src/lib/components/LeafletMap.svelte
@@ -20,9 +20,19 @@
     markers.forEach( marker => {
       leaflet.marker([marker.latitude, marker.longitude])
         .addTo(map)
-        .bindPopup(`<b>${marker.name}</b><br/>${marker.notes}`)
+        .bindPopup(`<b>${marker.name}</b><br/>${marker.notes}<br/>`)
+        .on('click', () => {
+          displayData(marker)
+        })
     })
+
     //TODO create a method that exposes the data so that upon clicking it gets displayed in a div next to the map component
+    //it's bound to marker.onclick above, now we just need to design and style the div. maybe do it directly on the map as a modal?
+    // but it would become too big if I add too much information. Add a scrolling bar in the modal?
+    const displayData = ( element ) => {
+      document.getElementById('marker_name').textContent = `${element.name}, ${element.notes}`
+      console.log(element.name)
+    }
   });
 
   onDestroy(async () => {
@@ -33,11 +43,13 @@
   });
 </script>
 
-<div bind:this={mapElement}></div>
+<div bind:this={mapElement} id="map"></div>
+
+<div id="marker_name"></div>
 
 <style>
     @import 'leaflet/dist/leaflet.css';
-    div {
+    #map {
       width: 100%;
       height: 100%;
       z-index: 1;
@@ -45,5 +57,9 @@
         border: none;
         border-radius: 0;
       }
+    }
+
+    #marker_name {
+      border: 1px solid red;
     }
 </style>
