d8492be51b81553972de06d2d9b6f679dbe9342c
Maratz <nicolas.moisson@protonmail.com>
2025-08-01 19:47:53 +0200

    🍱 map icons + 🚧 popup design and tweakings

--- src/routes/(inside)/souvenirs/+page.svelte
+++ src/routes/(inside)/souvenirs/+page.svelte
@@ -3,6 +3,10 @@
   import full_image from "$lib/assets/homepage/full_image.webp"
   import icone_medaille from "$lib/assets/icons/icone_medaille_60x60.png"
   import icone_penny from "$lib/assets/icons/icone_penny_60x60.png"
+  import icone_food from "$lib/assets/icons/food.svg"
+  import icone_bar from "$lib/assets/icons/beer.svg"
+  import icone_boutique from "$lib/assets/icons/shopping.svg"
+  import { formatDate, formatArray } from "$lib/utils/index.js";
 
   import { Map, TileLayer, Marker, Popup, Icon } from 'sveaflet'
 
@@ -54,7 +58,9 @@
         {#if souvenir.type === "médaille"}
           <Icon options={{
                   iconUrl: icone_medaille,
-                  popupAnchor: [15, 5] }}
+                  popupAnchor: [15, 5],
+                  iconSize: [25, 25],
+                  popupAnchor: [0, -5] }}
           />
         {:else if souvenir.type === "penny"}
           <Icon options={{
@@ -62,29 +68,59 @@
                   iconSize: [40, 40],
                   popupAnchor: [0, -5] }}
           />
+        {:else if souvenir.type === "restaurant"}
+          <Icon options={{
+                  iconUrl: icone_food,
+                  iconSize: [30, 30],
+                  popupAnchor: [0, -5] }}
+          />
+        {:else if souvenir.type === "bar"}
+          <Icon options={{
+                  iconUrl: icone_bar,
+                  iconSize: [35, 35],
+                  popupAnchor: [0, -5] }}
+          />
+        {:else if souvenir.type === "boutique"}
+          <Icon options={{
+                  iconUrl: icone_boutique,
+                  iconSize: [25, 25],
+                  popupAnchor: [0, -5] }}
+          />
         {/if}
         <Popup>
-          <div class="popup" class:flipped on:click="{() => flipped = !flipped}">
-
-            <div class="popup-image" id="image-{souvenir.id}" >
+          <div class="popup" >
+            {#if souvenir.image_recto !== null}
+              <div
+                class="popup-image"
+                id="image-{souvenir.id}"
+                class:flipped on:click="{() => flipped = !flipped}"
+                on:keypress={() => flipped = !flipped}
+                role="img"
+                aria-labelledby="image-face-{souvenir.id}">
 
                 <div class="popup-image-inner">
 
                   <div class="popup-image-front">
-                    <img src={souvenir.image_recto} alt="face">
+                    <img src={souvenir.image_recto} alt="Côté pile ou recto de la médaille ou du scan lié à ce souvenir" id="image-face-{souvenir.id}">
                   </div>
 
                   <div class="popup-image-back">
-                    <img src={souvenir.image_verso} alt="revers">
+                    <img src={souvenir.image_verso} alt="Côté face ou verso de la médaille ou du scan lié à ce souvenir">
                   </div>
 
                 </div>
 
             </div>
+            {/if}
 
             <div class="popup-content">
               <h2>{souvenir.name}</h2>
-              <p><i>{souvenir.type}</i></p>
+              <p><i>{souvenir.type}, visité {#if souvenir.date !== null}
+                le {formatDate(souvenir.date)}{:else}il y a quelques temps
+              {/if}</i></p>
+              {#if souvenir.people !== null}
+                <p><i>avec {formatArray(souvenir.people)}</i></p>
+              {/if}
               {#if souvenir.tagline !== (null || "")}
                 <p>{souvenir.tagline}</p>
               {/if}
@@ -130,28 +166,15 @@
   .popup {
     display: flex;
     flex-flow: column nowrap;
-    align-items: center;
     gap: 15px;
     width: fit-content;
     h2 {
       font-size: 1.3em;
       margin: 0 0;
+      padding-bottom: 5px;
     }
     p {
       margin: 0;
-    }
-    &-image {
-      background-color: transparent;
-      perspective: 1000px;
-      width: 140px;
-      height: 140px;
-      display: flex;
-      flex-flow: row nowrap;
-      position: relative;
-      outline: 1px solid red;
-      transition: transform 0.6s;
-      transform-style: preserve-3d;
-      cursor: pointer;
     }
     &-content {
       display: flex;
@@ -162,17 +185,45 @@
     }
   }
 
-  .flipped .popup-image {
+  .popup-image {
+    background-color: transparent;
+    perspective: 1000px;
+    width: 200px;
+    height: 200px;
+    display: flex;
+    flex-flow: row nowrap;
+    position: relative;
+    cursor: pointer;
+  }
+
+  .popup-image-inner {
+    width: 200px;
+    height: 200px;
+    position: relative;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+  }
+
+  .popup-image.flipped .popup-image-inner {
     transform: rotateY(180deg);
   }
 
   .popup-image-front,
   .popup-image-back {
     position: absolute;
-    width: 140px;
-    height: 140px;
+    width: 200px;
+    height: 200px;
     -webkit-backface-visibility: hidden;
     backface-visibility: hidden;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+  }
+
+  .popup-image-front img, .popup-image-back img {
+    width: auto;
+    height: 200px;
+    object-fit: contain;
   }
 
   .popup-image-back {
@@ -187,5 +238,9 @@
 
   :global(.leaflet-popup) {
     width: 250px;
+  }
+
+  p:has(+ p.long) {
+    padding-top: 5px;
   }
 </style>
