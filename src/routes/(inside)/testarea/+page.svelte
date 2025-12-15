d3f7e9c3263def1a9172c5af3707b5b6a1d1fd9a
Maratz <nicolas.moisson@protonmail.com>
2025-12-15 01:55:42 +0100

    🚧 removed TierList mention since it no longer exists

--- src/routes/(inside)/testarea/+page.svelte
+++ src/routes/(inside)/testarea/+page.svelte
@@ -1,14 +1,7 @@
 <script>
-  import TierList from "$lib/components/TierList.svelte"
-  import ItemTier from "$lib/components/ItemTier.svelte"
-  import { onMount } from "svelte"
 
-  let { data } = $props()
-  let tierlist = $state(data.tierlist[0])
-  console.log(tierlist)
 </script>
 
 <div>
-  <h1>Tier List</h1>
-  <TierList tierlistName={tierlist.name} tierSection={tierlist.tiers} tierlistID={tierlist.id}/>
+  <h1>Test</h1>
 </div>
