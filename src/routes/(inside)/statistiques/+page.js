7b92b0fb2800ea9eedc529b5f353a40ad355c77f
Maratzz <nicolas.moisson@protonmail.com>
2026-02-07 19:22:56 +0100

    📝 new blog post: questions en vrac #2

--- src/routes/(inside)/statistiques/+page.js
+++ src/routes/(inside)/statistiques/+page.js
@@ -3,7 +3,7 @@ export async function load({ parent }) {
 
   let { data : collection } = await supabase
     .from("collection")
-    .select("item_type,date_released,date_finished,status")
+    .select("item_type,date_released,date_finished,status,first_date_released")
     .order("date_created", { ascending: false })
 
   let { data : status } = await supabase
